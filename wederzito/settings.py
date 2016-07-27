08ace270d8d54def9ee7e03b045091aa4c998742
Ricardo Vitorino <me@ricardovitorino.com>
2016-07-27 23:43:53 +0100

    hotfix: RTFM #dj-database-url

--- wederzito/settings.py
+++ wederzito/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config()
-DATABASES['default'].update(os.environ.get('DATABASE_URL', ''))
+DATABASES['default'].update(db_from_env))
 
 
 # Internationalization
